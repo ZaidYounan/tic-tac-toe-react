fad3ea0236208aa93f4f44dae92d6a1a72b87981
ZaidYounan <zaid.younan@gmail.com>
2021-07-12 17:32:29 +1000

    Simplify class Square by converting into a function component as it only contains a render method and no state

--- src/index.js
+++ src/index.js
@@ -4,18 +4,13 @@ import './index.css';
 import App from './App';
 
 
-class Square extends React.Component {
-  render() {
+function Square(props) {
     return (
-      <button 
-      className="square" 
-      onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
+      <button className="square" onClick={props.onClick}>
+        {props.value}
       </button>
     );
   }
-}
 
 class Board extends React.Component {
   constructor(props) {
